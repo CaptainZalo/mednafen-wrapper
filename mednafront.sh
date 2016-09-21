44f06d49e49a0575b2aa9df50db4dcb98f9b8dbb
CaptainZalo <captzalo@gmail.com>
2016-09-21 16:30:34 +0200

    Update mednafront.sh

--- mednafront.sh
+++ mednafront.sh
@@ -6,15 +6,16 @@ folderpath=$(ls)
 
 system=$(eval zenity --list --column System $folderpath --width=480 --height=272)
 
+if [ "$?" -eq 1 ]; then
+echo "Cancelled."
+else
 cd $system
-
 files=$(ls -Q)
-
 rom=$(eval zenity --list --column "Roms" $files --width=480 --height=272)
 
-if [ $? == 1 ]; then
+if [ "$?" -eq 1 ]; then
 echo "Cancelled."
 else
 mednafen -fs 1 "$rom"
-
+fi
 fi
