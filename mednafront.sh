f8ae4eac22e2f2442a5fa2c601b7006748c727ed
CaptainZalo <captzalo@gmail.com>
2016-09-21 16:24:08 +0200

    Update mednafront.sh

--- mednafront.sh
+++ mednafront.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-filetype=$(zenity --entry)
 
-List=()
-while IFS= read -d $'\0' -r file ; do
-List=("${List[@]}" "$file")
-done < <(find . -name ''*.$filetype'' -print0)
+cd /home/chip/roms
 
-rom=$(zenity --list --column "Item" "${List[@]}")
+folderpath=$(ls)
+
+system=$(eval zenity --list --column System $folderpath --width=480 --height=272)
+
+cd $system
+
+files=$(ls -Q)
+
+rom=$(eval zenity --list --column "Roms" $files --width=480 --height=272)
 
 if [ $? == 1 ]; then
 echo "Cancelled."
 else
 mednafen -fs 1 "$rom"
+
 fi
